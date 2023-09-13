9c595f5c642b3e91401145b5ddd6e30764e256dd
Alexis Bonis <alexis.bonis1712@gmail.com>
2023-09-13 16:32:54 +0200

    add fichiers de base

---
+++ projet_mfreq_jd.py
@@ -0,0 +1,5 @@
+# faire une fonction qui prenne en entrée une image couleur ou grayscale
+# cette fonction doit tester le nombre de channel et traiter l'image en fonction
+# Paramètres : (Ismp, size_final, taille_patch (doit être impair), epsilon)
+
+import numpy as np
